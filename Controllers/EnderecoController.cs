5e20e83b7c36bf879f44bfe7016f74e83da679cb
Adao Ribeiro <sarinjr1024@hotmail.com>
2021-03-01 20:20:13 -0400

    API

---
+++ Controllers/EnderecoController.cs
@@ -0,0 +1,25 @@
+﻿using EnderecoApi.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace EnderecoApi.Controllers
+{
+    public class EnderecoController : ApiController
+    {
+        [Route("api/BuscarCEP/{cep}"), HttpGet]
+        public async Task<Endereco> Get(string cep)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync("http://viacep.com.br/ws/" + cep + "/json/");
+                var endereco = JsonConvert.DeserializeObject<Endereco>(response);
+                return endereco;
+            }
+        }
+
+            //asyb await  
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnderecoApi.WebApiApplication" Language="C#" %>
